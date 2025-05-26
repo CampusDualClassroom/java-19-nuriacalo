6555dfcdce745ceab943439b594f06b7bfa9dfca
Nuria Calo <nuriacalomosquera@gmail.com>
2025-05-26 12:07:49 +0200

    exercice 19 solved

--- src/main/java/com/campusdual/classroom/Exercise19.java
+++ src/main/java/com/campusdual/classroom/Exercise19.java
@@ -1,5 +1,7 @@
 package com.campusdual.classroom;
 
+import com.campusdual.util.Utils;
+
 public class Exercise19 {
 
     private static String getTridimensionalString(int[][][] intArrayTri, int[][] flatMatrix) {
@@ -12,18 +14,17 @@ public class Exercise19 {
             sb.append(stringTriMatrixRow(intArrayTri, i));
             sb.append("\n");
         }
-        sb.delete(sb.length()-1, sb.length());
+        sb.delete(sb.length() - 1, sb.length());
         return sb.toString();
-
     }
 
     private static String stringTriMatrixRow(int[][][] intArrayTri, int row) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < intArrayTri.length-1; i++){
+        for (int i = 0; i < intArrayTri.length - 1; i++) {
             sb.append(getUnidimensionalString(intArrayTri[i][row]));
             sb.append("   ");
         }
-        sb.append(getUnidimensionalString(intArrayTri[intArrayTri.length-1][row]));
+        sb.append(getUnidimensionalString(intArrayTri[intArrayTri.length - 1][row]));
 
         return sb.toString();
     }
@@ -39,33 +40,85 @@ public class Exercise19 {
 
     //TODO
     public static int[][] flatTridimensionalArray(int[][][] intArrayTri) {
-
+        int depth = intArrayTri.length;
+        int rows = intArrayTri[0].length;
+        int cols = intArrayTri[0][0].length;
+        int[][] result = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                for (int k = 0; k < depth; k++) {
+                    result[i][j] += intArrayTri[k][i][j];
+                }
+            }
+        }
+        return result;
     }
-
 
     // TODO
     public static String getBidimensionalString(int[][] intArrayBi) {
-
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < intArrayBi.length; i++) {
+            for (int j = 0; j < intArrayBi[i].length; j++) {
+                sb.append(intArrayBi[i][j]);
+                if (j < intArrayBi[i].length - 1) {
+                    sb.append(" ");
+                }
+            }
+            if (i < intArrayBi.length - 1) {
+                sb.append("\n");
+            }
+        }
+        return sb.toString();
     }
 
     // TODO
     public static String getUnidimensionalString(int[] uniArray) {
-
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < uniArray.length; i++) {
+            sb.append(uniArray[i]);
+            if (i < uniArray.length - 1) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
     }
+
 
     // TODO
     public static int[] createAndPopulateUnidimensionalArray(int columns) {
-
+        int[] unidimensionalArray = new int[columns];
+        for (int i = 0; i < columns; i++) {
+            unidimensionalArray[i] = i + 1;
+        }
+        return unidimensionalArray;
     }
 
     // TODO
     public static int[][] createAndPopulateBidimensionalArray(int rows, int columns) {
-
+        int[][] bidimensionalArray = new int[rows][columns];
+        int count = 1;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; j++) {
+                bidimensionalArray[i][j] = count;
+                count++;
+            }
+        }
+        return bidimensionalArray;
     }
 
     // TODO
     public static int[][][] createAndPopulateTridimensionalArray(int depth, int rows, int columns) {
-
+        int[][][] tridimensionalArray = new int[depth][rows][columns];
+        int count = 1;
+        for (int i = 0; i < depth; i++) {
+            for (int j = 0; j < rows; j++) {
+                for (int k = 0; k < columns; k++) {
+                    tridimensionalArray[i][j][k] = count;
+                    count++;
+                }
+            }
+        }
+        return tridimensionalArray;
     }
 
     public static void main(String[] args) {
